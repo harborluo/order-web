736b5796d9a4e4df5c164dd8d9c1b36d7edf1fee
harbor <85098287@qq.com>
2020-11-23 00:02:04 +0800

    remove file not in use

--- src/com/order/jdbc/ReportDao.java
+++
@@ -1,9 +0,0 @@
-package com.order.jdbc;
-
-import org.springframework.jdbc.support.rowset.SqlRowSet;
-import org.springframework.stereotype.Service;
-
-
-public interface ReportDao {
-	public SqlRowSet query(String sql, Object[] args);
-}
