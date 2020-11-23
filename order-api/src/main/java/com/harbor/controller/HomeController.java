6ac95bd42e769f63777a29189cf205a53543afd4
harbor <85098287@qq.com>
2020-11-24 00:06:39 +0800

    add layui for table list

---
+++ order-api/src/main/java/com/harbor/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.harbor.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage(){
+        log.info("go to home page: index.html");
+        return "index";
+    }
+}
