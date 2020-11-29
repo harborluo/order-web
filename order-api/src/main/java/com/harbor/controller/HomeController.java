66b68a244909fe6efaf7e42c4d9c01d855ef20bd
harbor <85098287@qq.com>
2020-11-29 12:49:53 +0800

    route to static html file

--- order-api/src/main/java/com/harbor/controller/HomeController.java
+++ order-api/src/main/java/com/harbor/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.harbor.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @Slf4j
@@ -12,5 +13,11 @@ public class HomeController {
     public String homePage(){
         log.info("go to home page: index.html");
         return "index";
+    }
+
+
+    @GetMapping("/view/{name}")
+    public String view(@PathVariable("name") String name){
+        return name;
     }
 }
